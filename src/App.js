43adf9a17ccace7d95843a0e710cabfd2c260b2b
tushargoyal812 <69681896+tushargoyal812@users.noreply.github.com>
2021-08-12 23:46:11 +0530

    added output style

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ export default function App() {
   const [number, setNumber] = useState();
   const [output, setOutput] = useState();
 
-  function checkHandler() {
+  function checkHandler(i) {
+    console.log(i);
+
+    i.preventDefault();
     var sum = 0;
     var birthdate = date.split("-");
     console.log(birthdate);
@@ -18,7 +21,7 @@ export default function App() {
     });
     console.log(sum);
     if (sum % Number(number) === 0) {
-      setOutput("you are lucky");
+      setOutput("you are lucky 😍🎉🎉");
     } else {
       setOutput("not lucky");
     }
@@ -28,30 +31,32 @@ export default function App() {
       <h1 className="heading">
         Enter Your Birthdate and lucky number to continue...
       </h1>
-      <div style={{ fontSize: "1.5rem", padding: "0.5rem" }}>
-        select your birth date
-      </div>
-      <input
-        className="dateInput"
-        type="date"
-        onChange={(e) => setDate(e.target.value)}
-        required="required"
-      />
-      <div style={{ fontSize: "1.5rem", padding: "0.5rem" }}>
-        enter your lucky number
-      </div>
-      <input
-        className="numberInput"
-        type="number"
-        onChange={(e) => setNumber(e.target.value)}
-        required="required"
-        min="0"
-      />
-      <div>
-        <button className="checkBtn" onClick={checkHandler}>
-          check
-        </button>
-      </div>
+      <form onSubmit={checkHandler}>
+        <div style={{ fontSize: "1.5rem", padding: "0.5rem" }}>
+          select your birth date
+        </div>
+        <input
+          className="dateInput"
+          type="date"
+          onChange={(e) => setDate(e.target.value)}
+          required
+        />
+        <div style={{ fontSize: "1.5rem", padding: "0.5rem" }}>
+          enter your lucky number
+        </div>
+        <input
+          className="numberInput"
+          type="number"
+          onChange={(e) => setNumber(e.target.value)}
+          required
+          min="0"
+        />
+        <div>
+          <button className="checkBtn" type="submit">
+            check
+          </button>
+        </div>
+      </form>
       <div>{output}</div>
     </div>
   );
