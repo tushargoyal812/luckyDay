d8639ff53c8dc717671f48f386f788a9bad48b02
tushargoyal812 <69681896+tushargoyal812@users.noreply.github.com>
2022-01-02 14:36:56 +0530

    linkedin address updated

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ export default function App() {
             </a>
           </li>
           <li className="list-item-inline">
-            <a href={"https://www.linkedin.com/in/tushar-goyal-8868741b1"}>
+            <a href={"https://www.linkedin.com/in/tushargoyal29/"}>
               <img className="linkedin-img" src={linkedinImg} />
             </a>
           </li>
